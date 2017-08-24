baf335965e7da3140c9e67a6a52ab5c29ccae341
aserranoni <30545071+aserranoni@users.noreply.github.com>
2017-08-24 18:32:39 -0300

    Add files via upload
    Versão mais nova do trabalho juntamente com alguns gráficos de teste

--- grepronto.r
+++ grepronto.r
@@ -2,6 +2,7 @@ library(readxl)
 library(stringr)
 library(dplyr)
 library(ggplot2)
+library(vcd)
 #leitura da tabela
 
 
@@ -48,9 +49,9 @@ for(i in 1:length(nums2)){
   
 ## matriz com os placares de cada jogador em cada jogada  
   
-  x <- data.frame(as.character(dados$playerAlias) , dados$move, dados$`optionChoosen`, dados$tree , dados$sequExecuted , dados$`movementTime(s)` , dados$correct, dados$game ,dados$phase)
+  x <- data.frame(as.character(dados$playerAlias) , dados$move, dados$`optionChoosen`, dados$tree , dados$sequExecuted , dados$`movementTime(s)` , dados$correct, dados$game ,dados$phase,dados$`Escala HY`,dados$Escolaridade)
 
- names(x) <- cbind("player","move","escolhido","tree","seqreal","tempo","acertou","jogo","fase")
+ names(x) <- cbind("player","move","escolhido","tree","seqreal","tempo","acertou","jogo","fase","escalaHY","escolaridade")
   
 player<-unique(x$player)
 score<-matrix(ncol=length(player),nrow = 200)
@@ -123,53 +124,95 @@ for ( i in 1:nrow (x) ) {
   
 }
 
-
-
-
-
 ############## Análise ###################
 
 jogos <- unique ( x$jogo )
+pdf("grafis.pdf" )
 
 #cada jogo
 for ( i in 1 : length(jogos) ) {
   
-  fases <- filter ( x, jogo == jogos[i] )
+  fases <- filter ( x, jogo== jogos[i] )
   
-  fases2<-unique(fases$player)
+  fases2<-unique(fases$fase)
 
-  #cada jogador  
+  #cada fase  
   for ( j in 1 : length(fases2) ){
+    tabela<-filter(fases , fase==fases2[j])
+    mediascore <- mean (tabela$vecs , na.rm = TRUE )
+    mediatempo <- mean ( tabela$tempo , na.rm = TRUE )
+    fases3<-unique(tabela$player)
+    mat<-matrix(nrow=length(fases3),ncol = 4)
     
-    tabjog <- filter ( x, jogo == jogos[i] )
-    
-    tabela <- filter ( tabjog, player == fases2 [j] ) 
-    
-    print (
+ #cada jogador
+    for(k in 1:length(fases3)){ 
       
-      ####colocar aqui o grafico a ser feito
-    #grafico aes =eixos 
-    ggplot(tabela, aes(x=move, y=tempo))  
-    #fala pra fazer pontos e colocar cores diferentes de acordo com o valor de acertou
-      +geom_point(aes(colour = factor(acertou)))
-    #curvinha maneira
-     #+geom_smooth()
-    #matriz de graficos
-    #+facet_grid (. ~fase )
-    #### nomes
-    +labs (title=str_c(as.character(fases2[j]), as.character(jogos[i]),sep=";") ,x="jogada", y="tempo" )
+    tabjog <-  filter(tabela, player==fases3[k])
+    
+    mej<-mean(tabjog$vecs , na.rm = TRUE)
+    
+    met<-mean( tabjog$tempo, na.rm=TRUE)
+    
+    mat[k,3]<-tabjog$escolaridade[1]
+    mat[k,4]<-tabjog$escalaHY[1]
     
     
-    ##
-     )
-  
+      if(mej >= mediascore && is.nan(mej)==FALSE ){
+      
+      mat[k,1]<-1
+      
+    }
+    else{
+      mat[k,1]<-0
+      
+    }
     
+    if(met<= mediatempo && is.nan(met)==FALSE ){
+      
+      mat[k,2]<-1
+    }
+    else{
+      mat[k,2]<-0
       }
-}
+    
+    
+   #GRAFICO
+      ####colocar aqui o grafico a ser feito
+      #grafico  aes=eixos
+      g<-ggplot(tabjog, aes(x=move, y=tempo) )
+      #fala pra fazer pontos e colocar cores diferentes de acordo com o valor de acerto
+      g<-g+geom_point(aes(color=factor(acertou)),rm.na=TRUE)
+       
+      #curvinha maneira
+      #+geom_smooth()
+      #matriz de graficos
+      #+facet_grid (. ~fase )
+      #### nomes
+    g<-g+labs (title=str_c(str_c(str_c(str_c(as.character(fases2[j]), as.character(jogos[i]),sep=";"),as.character(fases3[k]),sep=":"),as.character(tabjog$escalaHY[1]),sep = ":"),as.character(tabjog$escolaridade),sep=":"),x="jogada", y="tempo" )    
+   #fecha jogadodor
+      print(g)
+      
+      }
+#fecha fase
+    if(length(mat[,1])==length(mat[,2])){
+    nem<-table(as.factor(mat[,1]),as.factor(mat[,2]))
+    print(nem)
+   
+     #assoc(nem, nem[2,], nem[,2], shade=TRUE , legend=TRUE)
+     #as.matrix(nem)
+     mos<-mosaicplot(nem, main = "concentracao total",xlab = "pontuacao na fase",ylab = "tempo medio na fase",color = c(56,44))
+     print(mos)
+     #fecha jogo    
+     }
+    }  
+
+  }
+ 
+
+dev.off()
 
 
-
-
+#Analise
 
 
 
